8d99b63491ce702abd7e402ca2deebf956b8af38
Mike <rans@email.com>
2020-05-12 11:32:49 +0200

    Can handle indicator cols now

--- model/main.py
+++ model/main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from hdx.location.country import Country
 
 from model.admininfo import AdminInfo
--- model/tabularparser.py
+++ model/tabularparser.py
@@ -1,24 +1,44 @@
 # -*- coding: utf-8 -*-
+import logging
+
+from hdx.utilities.dictandlist import dict_of_lists_add
 from jsonpath_ng import parse
 
 from model import get_tabular_from_hdx
 from model.rowparser import RowParser
 
+logger = logging.getLogger(__name__)
 
-def get_tabular(adms, datasetinfo, iterator, retheaders=[list(), list()], retval=list()):
+
+def get_tabular(adms, name, datasetinfo, iterator, retheaders=[list(), list()], retval=list()):
     rowparser = RowParser(adms, datasetinfo)
-    valcols = datasetinfo['val_cols']
-    valuedicts = [dict() for _ in valcols]
+    indicatorcols = datasetinfo.get('indicator_cols')
+    if not indicatorcols:
+        indicatorcols = [{'filter_col': None, 'val_cols': datasetinfo['val_cols'], 'columns': datasetinfo['columns'],
+                          'hxltags': datasetinfo['hxltags']}]
+    valuedicts = dict()
+    for indicatorcol in indicatorcols:
+        for _ in indicatorcol['val_cols']:
+            dict_of_lists_add(valuedicts, indicatorcol['filter_col'], dict())
     for row in iterator:
         if not isinstance(row, dict):
             row = row.value
         adm = rowparser.do_set_value(row)
-        if adm:
-            for i, valcol in enumerate(valcols):
-                valuedicts[i][adm] = row[valcol]
-    retheaders[0].extend(datasetinfo['columns'])
-    retheaders[1].extend(datasetinfo['hxltags'])
-    retval.extend(valuedicts)
+        if not adm:
+            continue
+        for indicatorcol in indicatorcols:
+            filtercol = indicatorcol['filter_col']
+            if filtercol:
+                filter = filtercol.split('=')
+                if row[filter[0]] != filter[1]:
+                    continue
+            for i, valcol in enumerate(indicatorcol['val_cols']):
+                valuedicts[filtercol][i][adm] = row[valcol]
+    for indicatorcol in indicatorcols:
+        retheaders[0].extend(indicatorcol['columns'])
+        retheaders[1].extend(indicatorcol['hxltags'])
+        retval.extend(valuedicts[indicatorcol['filter_col']])
+    logger.info('Processed %s' % name)
     return retheaders, retval
 
 
@@ -37,18 +57,18 @@ def get_tabular_json(configuration, adms, downloader, national_subnational):
             iterator = expression.find(json)
         else:
             iterator = json
-        get_tabular(adms, datasetinfo, iterator, retheaders, retval)
+        get_tabular(adms, name, datasetinfo, iterator, retheaders, retval)
     return retheaders, retval
 
 
 def get_tabular_hdx(configuration, adms, national_subnational, downloader):
-    datasets = configuration['tabular_%s' % national_subnational]
+    datasets = configuration['tabular_hdx_%s' % national_subnational]
     retheaders = [list(), list()]
     retval = list()
     for name in datasets:
         datasetinfo = datasets[name]
         headers, iterator = get_tabular_from_hdx(downloader, datasetinfo)
-        get_tabular(adms, datasetinfo, iterator, retheaders, retval)
+        get_tabular(adms, name, datasetinfo, iterator, retheaders, retval)
     return retheaders, retval
 
 
