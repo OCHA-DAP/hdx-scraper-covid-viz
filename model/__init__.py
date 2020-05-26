142e569e86f7a0694e3d29aa8b95c45c9a7f9ff1
Mike <rans@email.com>
2020-05-26 17:34:04 +0200

    Output percentages as fractions

--- model/__init__.py
+++ model/__init__.py
@@ -10,6 +10,6 @@ today_str = today.strftime('%Y-%m-%d')
 def get_percent(numerator, denominator=None):
     if denominator:
         numerator /= denominator
-    return '%.0f%%' % (numerator * 100.0)
+    return '%.2f' % numerator
 
 
