e2bfdcf8902d3998d5e41ef96509991a12029d18
Mike <rans@email.com>
2020-06-02 07:57:26 +0200

    Update specific sheets option

--- model/main.py
+++ model/main.py
@@ -48,7 +48,7 @@ def extend_sources(sources, *args):
             sources.extend(arg)
 
 
-def get_indicators(configuration, downloader, scraper=None):
+def get_indicators(configuration, downloader, sheets, scraper=None):
     world = [list(), list()]
     national = [['iso3', 'countryname'], ['#country+code', '#country+name']]
     nationaltimeseries = [['iso3', 'date', 'indicator', 'value'], ['#country+code', '#date', '#indicator+name', '#indicator+value+num']]
@@ -58,29 +58,33 @@ def get_indicators(configuration, downloader, scraper=None):
     admininfo = AdminInfo.get()
     countryiso3s = admininfo.countryiso3s
 
-    fts_wheaders, fts_wcolumns, fts_wsources, fts_headers, fts_columns, fts_sources = get_fts(configuration, countryiso3s, downloader, scraper)
+    if 'world' in sheets or 'national' in sheets:
+        fts_wheaders, fts_wcolumns, fts_wsources, fts_headers, fts_columns, fts_sources = get_fts(configuration, countryiso3s, downloader, scraper)
 
-    extend_headers(world, fts_wheaders)
-    extend_wcolumns(world, fts_wcolumns)
-    extend_sources(sources, fts_wsources)
+        extend_headers(world, fts_wheaders)
+        extend_wcolumns(world, fts_wcolumns)
+        extend_sources(sources, fts_wsources)
 
-    tabular_headers, tabular_columns, tabular_sources = get_tabular(configuration, [countryiso3s], 'national', downloader, scraper)
-    copy_headers, copy_columns, copy_sources = get_copy(configuration, [countryiso3s], 'national', downloader, scraper)
+        if 'national' in sheets:
+            tabular_headers, tabular_columns, tabular_sources = get_tabular(configuration, [countryiso3s], 'national', downloader, scraper)
+            copy_headers, copy_columns, copy_sources = get_copy(configuration, [countryiso3s], 'national', downloader, scraper)
 
-    extend_headers(national, tabular_headers, fts_headers, copy_headers)
-    extend_columns(national, countryiso3s, None, tabular_columns, fts_columns, copy_columns)
-    extend_sources(sources, tabular_sources, fts_sources, copy_sources)
+            extend_headers(national, tabular_headers, fts_headers, copy_headers)
+            extend_columns(national, countryiso3s, None, tabular_columns, fts_columns, copy_columns)
+            extend_sources(sources, tabular_sources, fts_sources, copy_sources)
 
-    timeseries_sources = get_timeseries(nationaltimeseries, configuration, [countryiso3s], 'national', downloader, scraper)
-    extend_sources(sources, timeseries_sources)
+    if 'national_timeseries' in sheets:
+        timeseries_sources = get_timeseries(nationaltimeseries, configuration, [countryiso3s], 'national', downloader, scraper)
+        extend_sources(sources, timeseries_sources)
 
-    pcodes = admininfo.pcodes
-    tabular_headers, tabular_columns, tabular_sources = get_tabular(configuration, [countryiso3s, pcodes], 'subnational', downloader, scraper)
-    ipc_headers, ipc_columns, ipc_sources = get_ipc(configuration, admininfo, downloader, scraper)
-    whowhatwhere_headers, whowhatwhere_columns, whowhatwhere_sources = get_whowhatwhere(configuration, admininfo, downloader, scraper)
+    if 'subnational' in sheets:
+        pcodes = admininfo.pcodes
+        tabular_headers, tabular_columns, tabular_sources = get_tabular(configuration, [countryiso3s, pcodes], 'subnational', downloader, scraper)
+        ipc_headers, ipc_columns, ipc_sources = get_ipc(configuration, admininfo, downloader, scraper)
+        whowhatwhere_headers, whowhatwhere_columns, whowhatwhere_sources = get_whowhatwhere(configuration, admininfo, downloader, scraper)
 
-    extend_headers(subnational, tabular_headers, ipc_headers, whowhatwhere_headers)
-    extend_columns(subnational, pcodes, admininfo, tabular_columns, ipc_columns, whowhatwhere_columns)
-    extend_sources(sources, tabular_sources, ipc_sources, whowhatwhere_sources)
+        extend_headers(subnational, tabular_headers, ipc_headers, whowhatwhere_headers)
+        extend_columns(subnational, pcodes, admininfo, tabular_columns, ipc_columns, whowhatwhere_columns)
+        extend_sources(sources, tabular_sources, ipc_sources, whowhatwhere_sources)
 
     return world, national, nationaltimeseries, subnational, sources
--- run.py
+++ run.py
@@ -31,15 +31,24 @@ def parse_args():
     parser.add_argument('--test', action='store_true', help='Whether to output to test')
     parser.add_argument('--scratch', action='store_true', help='Whether to output to scratch')
     parser.add_argument('-sc', '--scraper', default=None, help='Scraper to run')
+    parser.add_argument('-us', '--updatesheets', default=None, help='Sheets to update')
     args = parser.parse_args()
     return args
 
 
-def main(gsheet_auth, test, scratch, scraper, **ignore):
+def main(gsheet_auth, test, scratch, scraper, updatesheets, **ignore):
     logger.info('##### hdx-scraper-covid-viz version %.1f ####' % VERSION)
     configuration = Configuration.read()
     with Download(rate_limit={'calls': 1, 'period': 1}) as downloader:
-        world, national, nationaltimeseries, subnational, sources = get_indicators(configuration, downloader, scraper)
+        sheets = configuration['sheets']
+        if updatesheets is None:
+            updatesheets = sheets.keys()
+            logger.info('Updating all sheets')
+        else:
+            logger.info('Updating only these sheets: %s' % updatesheets)
+        if scraper:
+            logger.info('Updating only scraper: %s' % scraper)
+        world, national, nationaltimeseries, subnational, sources = get_indicators(configuration, downloader, updatesheets, scraper)
         # Write to gsheets
         info = json.loads(gsheet_auth)
         scopes = ['https://www.googleapis.com/auth/spreadsheets']
@@ -52,21 +61,19 @@ def main(gsheet_auth, test, scratch, scraper, **ignore):
         else:
             url = configuration['prod_spreadsheet_url']
         spreadsheet = gc.open_by_url(url)
-        sheet = spreadsheet.worksheet_by_title(configuration['world_sheetname'])
-        sheet.clear(fields='*')
-        sheet.update_values('A1', world)
-        sheet = spreadsheet.worksheet_by_title(configuration['national_sheetname'])
-        sheet.clear(fields='*')
-        sheet.update_values('A1', national)
-        sheet = spreadsheet.worksheet_by_title(configuration['national_timeseries_sheetname'])
-        sheet.clear(fields='*')
-        sheet.update_values('A1', nationaltimeseries)
-        sheet = spreadsheet.worksheet_by_title(configuration['subnational_sheetname'])
-        sheet.clear(fields='*')
-        sheet.update_values('A1', subnational)
-        sheet = spreadsheet.worksheet_by_title(configuration['sources_sheetname'])
-        sheet.clear(fields='*')
-        sheet.update_values('A1', sources)
+
+        def update_sheet(sheetname, values):
+            if sheetname not in updatesheets:
+                return
+            sheet = spreadsheet.worksheet_by_title(sheets[sheetname])
+            sheet.clear(fields='*')
+            sheet.update_values('A1', values)
+
+        update_sheet('world', world)
+        update_sheet('national', national)
+        update_sheet('national_timeseries', nationaltimeseries)
+        update_sheet('subnational', subnational)
+        update_sheet('sources', sources)
 
 
 if __name__ == '__main__':
@@ -85,6 +92,10 @@ if __name__ == '__main__':
     gsheet_auth = args.gsheet_auth
     if gsheet_auth is None:
         gsheet_auth = getenv('GSHEET_AUTH')
+    if args.updatesheets:
+        updatesheets = args.updatesheets.split(',')
+    else:
+        updatesheets = None
     facade(main, hdx_read_only=True, user_agent=user_agent, preprefix=preprefix, hdx_site=hdx_site,
            project_config_yaml=join('config', 'project_configuration.yml'), gsheet_auth=gsheet_auth, test=args.test,
-           scratch=args.scratch, scraper=args.scraper)
+           scratch=args.scratch, scraper=args.scraper, updatesheets=updatesheets)
