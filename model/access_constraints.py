faf6a1c748ca5d77932c3c082e2bfa82d62fbf3b
Mike <rans@email.com>
2020-07-21 20:34:38 +0200

    New access severity calc

--- model/access_constraints.py
+++ model/access_constraints.py
@@ -42,23 +42,18 @@ def get_access(configuration, countryiso3s, downloader, scraper=None):
             countrydata = datasheet.get(countryiso, dict())
             score = countrydata.get('score', 0)
             newscore = row[sheetinfo['scorecol']]
-            scoring = sheetinfo.get('scoring')
-            if scoring:
-                 newscore = scoring[newscore.lower()]
             textcol = sheetinfo.get('textcol')
             if textcol:
-                dict_of_lists_add(countrydata, 'text', (newscore, row[textcol]))
+                text = row[textcol]
+                dict_of_lists_add(countrydata, 'text', (newscore, text))
+                weights = sheetinfo.get('weights')
+                if weights:
+                    weight = weights.get(text)
+                    if weight:
+                        newscore *= weight
             score += newscore
             countrydata['score'] = score
             datasheet[countryiso] = countrydata
-        weight = sheetinfo['weight']
-        ranges = sheetinfo.get('ranges')
-        for countryiso in datasheet:
-            countrydata = datasheet[countryiso]
-            score = countrydata['score']
-            if ranges:
-                score = process_range(ranges, score)
-            countrydata['score'] = weight * score
         data[sheet] = datasheet
     valuedicts = [dict() for _ in range(5)]
     severityscore = valuedicts[0]
@@ -66,20 +61,17 @@ def get_access(configuration, countryiso3s, downloader, scraper=None):
         datasheet = data[sheet]
         for countryiso in datasheet:
             countrydata = datasheet[countryiso]
-            score = severityscore.get(countryiso, 0)
-            if i < 3:
-                ranked = sorted(countrydata['text'], reverse=True)
-                top_value = ranked[0][0]
-                texts = list()
-                for value, text in countrydata['text']:
-                    if value == top_value:
-                        texts.append(text)
-                valuedicts[i+2][countryiso] = '|'.join(texts)
-            if sheet == 'mitigation':
-                score -= countrydata['score']
-            else:
+            ranked = sorted(countrydata['text'], reverse=True)
+            top_value = ranked[0][0]
+            texts = list()
+            for value, text in countrydata['text']:
+                if value == top_value:
+                    texts.append(text)
+            valuedicts[i+2][countryiso] = '|'.join(texts)
+            if sheet != 'impact':
+                score = severityscore.get(countryiso, 0)
                 score += countrydata['score']
-            severityscore[countryiso] = score
+                severityscore[countryiso] = score
     ranges = access_configuration['category']
     severitycategory = valuedicts[1]
     for countryiso in severityscore:
