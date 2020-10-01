11d37b4658862d5fa8c22f2e102eebf46a5e3d15
Mike <rans@email.com>
2020-10-01 17:13:51 +0200

    Test for date_condition

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,14 +29,14 @@ class TestCovid:
         return join('tests', 'fixtures')
 
     def test_get_indicators(self, configuration, folder):
-        with temp_dir('TestCovidViz', delete_on_success=False, delete_on_failure=False) as tempdir:
+        with temp_dir('TestCovidViz', delete_on_success=True, delete_on_failure=True) as tempdir:
             with Download(user_agent='test') as downloader:
                 tabs = configuration['tabs']
                 noout = nooutput(tabs)
                 jsonout = jsonoutput(configuration, tabs)
                 outputs = {'gsheets': noout, 'excel': noout, 'json': jsonout}
                 admininfo = AdminInfo.setup(downloader)
-                get_indicators(configuration, downloader, admininfo, outputs, tabs, scrapers=['ifi', 'who_global', 'who_national', 'who_subnational', 'who_covid', 'sadd'])
+                get_indicators(configuration, downloader, admininfo, outputs, tabs, scrapers=['ifi', 'who_global', 'who_national', 'who_subnational', 'who_covid', 'sadd', 'covidtests'])
                 filepaths = jsonout.save(tempdir, hrp_iso3s=admininfo.hrp_iso3s)
                 assert filecmp.cmp(filepaths[0], join(folder, 'test_tabular.json'))
                 assert filecmp.cmp(filepaths[1], join(folder, 'test_tabular_covidseries.json'))
