144137aa18b680e0b16a07b545b9874844827412
Mike <rans@un.org>
2021-02-11 08:32:17 +1300

    Change vaccination campaigns calculation

--- model/vaccination_campaigns.py
+++ model/vaccination_campaigns.py
@@ -20,23 +20,25 @@ def add_vaccination_campaigns(configuration, today, countryiso3s, downloader, ou
     for row in iterator:
         newrow = dict()
         countryiso = None
+        status = None
         for key in row:
             hxltag = hxlrow[key]
-            if hxltag != '':
-                value = row[key]
-                newrow[hxlrow[key]] = value
-                if hxltag == '#country+code':
-                    countryiso = value
-                    if countryiso not in countryiso3s:
-                        countryiso = None
-                        break
-                    campaigns_per_country[countryiso] = campaigns_per_country.get(countryiso, 0) + 1
-                if hxltag == '#status+name':
-                    value = value.lower()
-                    if value != 'on track' and 'reinstated' not in value:
-                        affected_campaigns_per_country[countryiso] = affected_campaigns_per_country.get(countryiso, 0) + 1
-        if countryiso:
-            outputs['json'].add_data_row(name, newrow)
+            if hxltag == '':
+                continue
+            value = row[key]
+            newrow[hxlrow[key]] = value
+            if hxltag == '#country+code':
+                countryiso = value
+            elif hxltag == '#status+name':
+                status = value.lower()
+        if not countryiso or countryiso not in countryiso3s:
+            continue
+        if not status or status == 'completed as planned':
+            continue
+        outputs['json'].add_data_row(name, newrow)
+        campaigns_per_country[countryiso] = campaigns_per_country.get(countryiso, 0) + 1
+        if status != 'on track' and 'reinstated' not in status:
+            affected_campaigns_per_country[countryiso] = affected_campaigns_per_country.get(countryiso, 0) + 1
     ratios = calculate_ratios(campaigns_per_country, affected_campaigns_per_country)
     hxltag = '#vaccination+num+ratio'
     logger.info('Processed vaccination campaigns')
