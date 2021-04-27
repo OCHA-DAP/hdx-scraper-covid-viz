979976dfb09a4cbc240926880a45455b18c687b6
Mike <rans@un.org>
2021-04-28 05:40:41 +1200

    Change weekly_new_cases_pct_change, weekly_new_deaths_pct_change to be fractions

--- scrapers/who_covid.py
+++ scrapers/who_covid.py
@@ -154,16 +154,14 @@ def get_who_covid(configuration, today, outputs, hrp_countries, gho_countries, r
         columns=['Country Code'])
     # Get cases per hundred thousand
     output_df = output_df.rename(columns={'New_cases': 'weekly_new_cases', 'New_deaths': 'weekly_new_deaths',
+                                          'NewCase_PercentChange': 'weekly_new_cases_pc_change', 'NewDeath_PercentChange': 'weekly_new_deaths_pc_change',
                                           'diff_cases': 'weekly_new_cases_change', 'diff_deaths': 'weekly_new_deaths_change',
                                           'Cumulative_cases': 'cumulative_cases', 'Cumulative_deaths': 'cumulative_deaths'})
     output_df['weekly_new_cases_per_ht'] = output_df['weekly_new_cases'] / output_df['population'] * 1E5
     output_df['weekly_new_deaths_per_ht'] = output_df['weekly_new_deaths'] / output_df['population'] * 1E5
-    output_df['weekly_new_cases_pc_change'] = output_df['NewCase_PercentChange'] * 100
-    output_df['weekly_new_deaths_pc_change'] = output_df['NewDeath_PercentChange'] * 100
 
     output_df['Date_reported'] = output_df['Date_reported'].apply(lambda x: x.strftime('%Y-%m-%d'))
-    output_df = output_df.drop(
-        ['NewCase_PercentChange', 'NewDeath_PercentChange', 'ndays'], axis=1)
+    output_df = output_df.drop(['ndays'], axis=1)
     trend_hxltags = {'ISO_3_CODE': '#country+code', 'Date_reported': '#date+reported',
                      'weekly_new_cases': '#affected+infected+new+weekly', 'weekly_new_deaths': '#affected+killed+new+weekly',
                      'weekly_new_cases_per_ht': '#affected+infected+new+per100000+weekly', 'weekly_new_deaths_per_ht': '#affected+killed+new+per100000+weekly',
